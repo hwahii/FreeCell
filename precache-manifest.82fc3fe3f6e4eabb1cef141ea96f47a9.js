3b1798fba804497ef3041a80968ef40686cf49ad
Huang Yin Chung <in1102@livemail.tw>
2019-07-24 01:09:34 +0800

    Updates

---
+++ precache-manifest.82fc3fe3f6e4eabb1cef141ea96f47a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d795c7f6d7f2c11bbe3cc812d82a816b",
+    "url": "/freecell/index.html"
+  },
+  {
+    "revision": "a55288e61110072a0d11",
+    "url": "/freecell/static/css/main.181e0038.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/freecell/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "a55288e61110072a0d11",
+    "url": "/freecell/static/js/main.3375618e.chunk.js"
+  },
+  {
+    "revision": "04cd019370f62e149493",
+    "url": "/freecell/static/js/runtime~main.10e4263b.js"
+  }
+]);
